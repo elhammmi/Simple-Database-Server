41d9ba6c2300d45b039ffe14520c1a332824858b
Elham Mirzavand <elham.mirzavand@gmail.com>
2023-09-23 18:07:33 +0200

    Simple 1

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const host = '127.0.0.1';
 //Create an instance of the server
 const server = net.createServer();
 //Start listening with the server on given port and host.
+//log 1
 server.listen(port,host,function(){
    console.log(`Server started on ${host}:${port}`); 
 });
