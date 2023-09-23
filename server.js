8cad55b4834ed97516bed8738c1e1c110f625096
Elham Mirzavand <elham.mirzavand@gmail.com>
2023-09-23 18:00:13 +0200

    create tcp server

---
+++ server.js
@@ -0,0 +1,10 @@
+const net = require('net');
+//define host and port to run the server
+const port = 8080;
+const host = '127.0.0.1';
+//Create an instance of the server
+const server = net.createServer();
+//Start listening with the server on given port and host.
+server.listen(port,host,function(){
+   console.log(`Server started on ${host}:${port}`); 
+});
